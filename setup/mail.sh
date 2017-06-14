5af5939862e9f19b017b9bff0cdc3fbd30a0ee91
Megh Parikh <meghprkh@gmail.com>
2017-06-15 01:46:05 +0530

    Remove mailcatcher hack and use new config settings

--- setup/mail.sh
+++ setup/mail.sh
@@ -1,4 +1,1 @@
-cd src/app/libraries/
-sed -i 's/\$mail->SMTPAuth   = true;//g' email_lib.php
-sed -i 's/\$mail->SMTPSecure = "tls";//g' email_lib.php
 echo "mailcatcher --http-ip=0.0.0.0" >>~/.bashrc
