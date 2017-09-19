c3ac32697b2a01a309b28445767acdf2778e36c4
Megh Parikh <meghprkh@gmail.com>
2017-09-19 19:07:21 +0530

    Force cloning of master branch of repo irrespective of default branch

--- setup/otse.sh
+++ setup/otse.sh
@@ -4,5 +4,5 @@ cd /var/www
 # Remove the next two lines if you are not behind IIIT proxy
 cat setup/proxyenv.sh | cat >>/home/vagrant/.bashrc
 source setup/proxyenv.sh
-git clone --recursive https://github.com/felicity-iiith/felicity18-website.git
+git clone --recursive -b master https://github.com/felicity-iiith/felicity18-website.git
 bash /var/www/setup.sh
