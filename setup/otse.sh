afffb52422c6c47dbf986265b4f93b5f57106b0c
Megh Parikh <meghprkh@gmail.com>
2018-12-18 13:21:45 +0530

    Remove proxy coz transparent proxy

--- setup/otse.sh
+++ setup/otse.sh
@@ -4,7 +4,7 @@ DIR=felicity-website
 
 cd /var/www
 # Remove the next two lines if you are not behind IIIT proxy
-cat setup/proxyenv.sh | cat >>/home/vagrant/.bashrc
-source setup/proxyenv.sh
+# cat setup/proxyenv.sh | cat >>/home/vagrant/.bashrc
+# source setup/proxyenv.sh
 git clone --recursive -b master https://github.com/felicity-iiith/felicity18-website.git $DIR
 bash /var/www/setup/setup.sh
